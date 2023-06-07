82d4835bd653687c918d20a9ee9a9a62e8b60c19
Odeyemi Tobiloba <odeyemioluwatobiloba11@gmail.com>
2023-06-07 14:38:52 +0100

    Ok

--- src/Pages/main/Home.jsx
+++ src/Pages/main/Home.jsx
@@ -94,10 +94,31 @@ function Home() {
         <div className="bg-[#d4a473]">
           <div className="py-[4rem] ">
             <div className="text-center">
-              <h1 className="text-2xl sm:text-[3rem] text-white font-semibold">ABOUT</h1>
+              <h1 className="text-2xl sm:text-[3rem] text-white font-semibold">
+                ABOUT
+              </h1>
             </div>
           </div>
         </div>
+
+        <div className="bg-[#432818]">
+          <div className="py-9 ">
+            <div className="text-center text-white">
+              <div>
+                <h1>Contact Us</h1>
+              </div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+
+        <footer className="bg-[#432818] border-t-2 border-[#fff6e4]">
+          <div className="text-center place-items-center grid h-[8vh]">
+            <h1 className="text-base text-white">
+              &copy; Copyright 2023. Asooke by Abimsaro.
+            </h1>
+          </div>
+        </footer>
       </div>
     </Mainlayout>
   );
--- src/Pages/sub/Hub.jsx
+++ src/Pages/sub/Hub.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import As1 from "../../Assets/pic/Asooke1.png";
 import As2 from "../../Assets/pic/Asooke2.png";
 import As3 from "../../Assets/pic/Asooke3.png";
 import As4 from "../../Assets/pic/Asooke4.png";
@@ -11,6 +12,37 @@ import As10 from "../../Assets/pic/Asooke10.png";
 import As11 from "../../Assets/pic/Asooke11.png";
 import As12 from "../../Assets/pic/Asooke12.png";
 import As13 from "../../Assets/pic/Asooke13.png";
+import As14 from "../../Assets/pic/Asooke14.png";
+import As15 from "../../Assets/pic/Asooke15.png";
+import As16 from "../../Assets/pic/Asooke16.png";
+import As17 from "../../Assets/pic/Asooke17.png";
+import As18 from "../../Assets/pic/Asooke18.png";
+import As19 from "../../Assets/pic/Asooke19.png";
+import As20 from "../../Assets/pic/Asooke20.png";
+import As21 from "../../Assets/pic/Asooke21.png";
+import As22 from "../../Assets/pic/Asooke22.png";
+import As23 from "../../Assets/pic/Asooke23.png";
+import As24 from "../../Assets/pic/Asooke24.png";
+import As25 from "../../Assets/pic/Asooke25.png";
+import As26 from "../../Assets/pic/Asooke26.png";
+import As27 from "../../Assets/pic/Asooke27.png";
+import As28 from "../../Assets/pic/Asooke28.png";
+import As29 from "../../Assets/pic/Asooke29.png";
+import As30 from "../../Assets/pic/Asooke30.png";
+import As31 from "../../Assets/pic/Asooke31.png";
+import As32 from "../../Assets/pic/Asooke32.png";
+import As33 from "../../Assets/pic/Asooke33.png";
+import As34 from "../../Assets/pic/Asooke34.png";
+import As35 from "../../Assets/pic/Asooke35.png";
+import As36 from "../../Assets/pic/Asooke36.png";
+import As37 from "../../Assets/pic/Asooke37.png";
+import As38 from "../../Assets/pic/Asooke38.png";
+import As39 from "../../Assets/pic/Asooke39.png";
+import As40 from "../../Assets/pic/Asooke40.png";
+import As41 from "../../Assets/pic/Asooke41.png";
+import As42 from "../../Assets/pic/Asooke42.png";
+import As43 from "../../Assets/pic/Asooke43.png";
+import As44 from "../../Assets/pic/Asooke44.jpg";
 
 import Mainlayout from "../../Layouts/Mainlayout";
 
@@ -20,73 +52,271 @@ function Hub() {
     {
       id: 1,
       image: (
-        <img className="rounded-[10px] xl:h-[25rem]" src={As2} alt="As2" />
+        <img className="rounded-[10px] xl:h-[25rem]" src={As1} alt="Asooke" />
+      ),
+    },
+    {
+      id: 1,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As2} alt="Asooke" />
       ),
     },
     {
       id: 2,
       image: (
-        <img className="rounded-[10px] xl:h-[25rem]" src={As3} alt="As2" />
+        <img className="rounded-[10px] xl:h-[25rem]" src={As3} alt="Asooke" />
       ),
     },
     {
       id: 3,
       image: (
-        <img className="rounded-[10px] xl:h-[25rem]" src={As4} alt="As2" />
+        <img className="rounded-[10px] xl:h-[25rem]" src={As4} alt="Asooke" />
       ),
     },
     {
       id: 4,
       image: (
-        <img className="rounded-[10px] xl:h-[25rem]" src={As5} alt="As2" />
+        <img className="rounded-[10px] xl:h-[25rem]" src={As5} alt="Asooke" />
       ),
     },
     {
       id: 5,
       image: (
-        <img className="rounded-[10px] xl:h-[25rem]" src={As6} alt="As2" />
+        <img className="rounded-[10px] xl:h-[25rem]" src={As6} alt="Asooke" />
       ),
     },
     {
       id: 6,
       image: (
-        <img className="rounded-[10px] xl:h-[25rem]" src={As7} alt="As2" />
+        <img className="rounded-[10px] xl:h-[25rem]" src={As7} alt="Asooke" />
       ),
     },
     {
       id: 6,
       image: (
-        <img className="rounded-[10px] xl:h-[25rem]" src={As8} alt="As2" />
+        <img className="rounded-[10px] xl:h-[25rem]" src={As8} alt="Asooke" />
       ),
     },
     {
       id: 6,
       image: (
-        <img className="rounded-[10px] xl:h-[25rem]" src={As9} alt="As2" />
+        <img className="rounded-[10px] xl:h-[25rem]" src={As9} alt="Asooke" />
       ),
     },
     {
       id: 6,
       image: (
-        <img className="rounded-[10px] xl:h-[25rem]" src={As10} alt="As2" />
+        <img className="rounded-[10px] xl:h-[25rem]" src={As10} alt="Asooke" />
       ),
     },
     {
       id: 6,
       image: (
-        <img className="rounded-[10px] xl:h-[25rem]" src={As11} alt="As2" />
+        <img className="rounded-[10px] xl:h-[25rem]" src={As11} alt="Asooke" />
       ),
     },
     {
       id: 6,
       image: (
-        <img className="rounded-[10px] xl:h-[25rem]" src={As12} alt="As2" />
+        <img className="rounded-[10px] xl:h-[25rem]" src={As12} alt="Asooke" />
       ),
     },
     {
       id: 6,
       image: (
-        <img className="rounded-[10px] xl:h-[25rem]" src={As13} alt="As2" />
+        <img className="rounded-[10px] xl:h-[25rem]" src={As13} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As14} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As15} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As16} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As17} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As18} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As19} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As20} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As21} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As22} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As23} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As24} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As25} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As26} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As27} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As28} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As29} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As30} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As31} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As32} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As33} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As34} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As35} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As36} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As37} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As38} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As39} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As40} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As41} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As42} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As43} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As44} alt="Asooke" />
+      ),
+    },
+    {
+      id: 6,
+      image: (
+        <img className="rounded-[10px] xl:h-[25rem]" src={As24} alt="Asooke" />
       ),
     },
   ];
@@ -104,7 +334,7 @@ function Hub() {
           <div className=" xl:px-[18rem] sm:px-4 px-2 md:py-20 py-8 grid grid-cols-2 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 gap-10">
             {service?.map((service) => (
               <div className=" w-full">
-                <div className=" place-items-center grid">{service.image}</div>
+                <div className=" cursor-pointer place-items-center grid">{service.image}</div>
               </div>
             ))}
           </div>
